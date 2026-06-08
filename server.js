1ef25b8c4610e6260b021257d39a9edd5bff8f62
creative-a <62901137+creative-a@users.noreply.github.com>
2026-06-08 16:02:12 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,66 @@
+
+
+const express = require('express');
+const { createClient } = require('@supabase/supabase-js');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// SupabaseTok
+const SUPABASE_URL = 'https://gqqkrybgjpepxjhllbbl.supabase.co/rest/v1/';
+const SUPABASE_KEY = 'sb_publishable_nxzUw1jskT2DAxkkffukLA_YGRHgoB9';
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+app.use(express.json());
+app.use(express.static(__dirname));
+
+// 1. retrieve
+app.get('/api/items', async (req, res) => {
+    const { data, error } = await supabase.from('quotation_items').select('*');
+    if (error) return res.status(500).json(error);
+    
+    // edit columns
+const formattedData = data.map(item => ({
+        id: item.id, 
+        category: item.category, 
+        name: item.name, 
+        details: item.details,
+        source: item.source, 
+        phone: item.phone, 
+        qty: item.qty, 
+        unit: item.unit,
+        price: item.price, 
+        isArchived: item.is_archived,
+        createdAt: item.created_at || '---' // date
+    }));
+    res.json(formattedData);
+});
+
+// 2. synqr
+app.post('/api/save', async (req, res) => {
+    const items = req.body;
+    
+    // supabase SQL
+const dbRows = items.map(item => ({
+        id: item.id, 
+        category: item.category, 
+        name: item.name, 
+        details: item.details,
+        source: item.source, 
+        phone: item.phone, 
+        qty: item.qty, 
+        unit: item.unit,
+        price: item.price, 
+        is_archived: item.isArchived,
+        created_at: item.createdAt 
+    }));
+
+    // مسح البيانات القديمة وإدخال الجديدة (Upsert) لضمان التحديث التلقائي
+const { error } = await supabase.from('quotation_items').upsert(dbRows);
+    if (error) return res.status(500).json(error);
+    
+    res.json({ success: true });
+});
+
+app.listen(PORT, () => console.log(` السيرفر جاهز على منفذ ${PORT}`));
